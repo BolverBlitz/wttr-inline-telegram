b1a66fbe5cf145ccf65eb254e317fc9d8dad28c3
Marc <bolverblitz@ebg.pw>
2021-10-12 04:12:42 +0200

    Inline Funktion

--- src/telegram.js
+++ src/telegram.js
@@ -1,5 +1,5 @@
 const util = require('util')
-const DB = require('../lib/db/postgres');
+//const DB = require('../lib/db/postgres');
 const path = require('path');
 const reqPath = path.join(__dirname, '../');
 const { default: i18n } = require('new-i18n');
@@ -10,4 +10,52 @@ const bot = new Telebot({
 	token: process.env.TG_BotToken,
 	limit: 1000,
         usePlugins: ['commandButton', 'askUser']
+});
+
+bot.start(); //Telegram bot start
+
+bot.on('inlineQuery', msg => {
+	
+	const answers = bot.answerList(msg.id, {cacheTime: 1});
+	
+	if(msg.hasOwnProperty('location')){
+		let lat = msg.location.latitude.toFixed(2)
+		let lon = msg.location.longitude.toFixed(2)
+
+		// Article
+		answers.addPhoto({
+			id: '1',
+			thumb_url: `https://wttr.in/${lat},${lon}.png?lang=en`,
+			photo_url: `https://wttr.in/${lat},${lon}.png?lang=en`
+		});
+
+		return bot.answerQuery(answers);
+	}else{
+		if(msg.query.length > 1){
+			
+			answers.addPhoto({
+				id: '1',
+				thumb_url: `https://wttr.in/${msg.query}.png?lang=en`,
+				photo_url: `https://wttr.in/${msg.query}.png?lang=en`
+			});
+			/* Does not work with TG for some reason lol
+			answers.addPhoto({
+				id: '2',
+				thumb_url: `https://v2.wttr.in/${msg.query}.png?lang=en`,
+				photo_url: `https://v2.wttr.in/${msg.query}.png?lang=en`
+			});
+			*/
+			return bot.answerQuery(answers);
+		}else{
+			Message = newi18n.translate("Inline.NoGPSAndQuery")
+			answers.addArticle({
+				id: 'GPS',
+				title: newi18n.translate("Inline.NoGPSAndQueryTitle"),
+				description: newi18n.translate("Inline.NoGPSAndQuerydescription"),
+				message_text: (Message)
+			});
+			return bot.answerQuery(answers);
+		}
+	}
+
 });
