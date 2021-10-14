128cf72f33b1fbabb4b2c049ac61cc581bc1b463
Marc <bolverblitz@ebg.pw>
2021-10-14 04:41:12 +0200

    + /setup command

--- src/telegram.js
+++ src/telegram.js
@@ -1,5 +1,5 @@
 const util = require('util')
-//const DB = require('../lib/db/postgres');
+const DB = require('../lib/db/postgres');
 const path = require('path');
 const reqPath = path.join(__dirname, '../');
 const { default: i18n } = require('new-i18n');
@@ -22,6 +22,29 @@ bot.on(/^\/start$/i, (msg) => {
 bot.on(/^\/help$/i, (msg) => {
 	let helpmsg = newi18n.translate("helpmsg")
 	msg.reply.text(helpmsg, { parseMode: 'html' });
+});
+
+bot.on(/^\/setup( .+)*/i, (msg, props) => {
+	let CheckAtributes = AtrbutCheck(props);
+	if(!CheckAtributes.hasAtributes){
+		bot.sendMessage(msg.chat.id, newi18n.translate("setup.help"), {parseMode: 'html'});
+	}else{
+		let private;
+		if(msg.chat){
+			if(msg.chat.type === "private"){private = true}
+		}else{
+			if(msg.message.chat.type === "private"){private = true}
+		}
+		if(!private){
+			if(isChatAdmin(msg)){
+				
+			}else{
+				bot.sendMessage(msg.chat.id, newi18n.translate("setup.nopermissions"), {parseMode: 'html'})
+			}
+		}else{
+
+		}
+	}
 });
 
 bot.on('inlineQuery', msg => {
@@ -69,3 +92,43 @@ bot.on('inlineQuery', msg => {
 	}
 
 });
+
+function AtrbutCheck(props) {
+	let input = props.match.input.split(' ')
+	if(input[0].endsWith(process.env.Telegram_Bot_Botname)){
+		let atributesWName = [];
+		for(let i=1; i <= input.length - 1; i++){
+			atributesWName.push(input[i])
+		}
+		if(atributesWName.length >= 1){
+			return {hasAtributes: true, atributes: atributesWName}
+		}else{
+			return {hasAtributes: false}
+		}
+	}else{
+		if(typeof(props.match[1]) === 'undefined'){
+			return {hasAtributes: false}
+		}else{
+			let atributeOName = [];
+			let input = props.match[1].split(' ')
+			for(let i=1; i <= input.length - 1; i++){
+				atributeOName.push(input[i])
+			}
+			return {hasAtributes: true, atributes: atributeOName}
+		}
+	}
+}
+
+function isChatAdmin(msg) {
+	bot.getChatAdministrators(msg.chat.id).then(function(Admins) {
+		AdminArray = [];
+		for(i in Admins){
+			AdminArray.push(Admins[i].user.id)
+		}
+		if(AdminArray.includes(msg.from.id)){
+			return true
+		}else{
+			return false
+		}
+	});
+}
